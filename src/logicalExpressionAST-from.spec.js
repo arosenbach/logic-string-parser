74539ebb663d50facbae77b032382513ba9d2e03
Antoine Rosenbach <github@rbch.ovh>
2023-04-30 08:56:04 +0200

    refactor tests

--- src/logicalExpressionAST-from.spec.js
+++ src/logicalExpressionAST-from.spec.js
@@ -1,16 +1,71 @@
 import LogicalExpressionAST from "./logicalExpressionAST";
 
-describe("LogicalExpressionAST", () => {
-  describe("from()", () => {
-    it("parses a simple logic string", () => {
-      expect(LogicalExpressionAST.from("1")).toEqual({
+describe("LogicalExpressionAST - from()", () => {
+  it("parses a simple logical expression", () => {
+    expect(LogicalExpressionAST.from("1")).toEqual({
+      type: "literal",
+      value: 1,
+    });
+  });
+
+  it("parses a simple AND logical expression", () => {
+    expect(LogicalExpressionAST.from("1 AND 2")).toEqual({
+      type: "AND",
+      left: {
         type: "literal",
         value: 1,
-      });
+      },
+      right: {
+        type: "literal",
+        value: 2,
+      },
     });
+  });
 
-    it("parses a simple AND logic string", () => {
-      expect(LogicalExpressionAST.from("1 AND 2")).toEqual({
+  it("parses a simple OR logical expression", () => {
+    expect(LogicalExpressionAST.from("1 OR 2")).toEqual({
+      type: "OR",
+      left: {
+        type: "literal",
+        value: 1,
+      },
+      right: {
+        type: "literal",
+        value: 2,
+      },
+    });
+  });
+
+  it("parses multiple OR", () => {
+    expect(LogicalExpressionAST.from("1 OR 2 OR 3")).toEqual({
+      type: "OR",
+      left: { type: "literal", value: 1 },
+      right: {
+        left: { type: "literal", value: 2 },
+        right: { type: "literal", value: 3 },
+        type: "OR",
+      },
+    });
+  });
+
+  it("parses a simple logical expression with parentheses", () => {
+    expect(LogicalExpressionAST.from("(1 OR 2)")).toEqual({
+      type: "OR",
+      left: {
+        type: "literal",
+        value: 1,
+      },
+      right: {
+        type: "literal",
+        value: 2,
+      },
+    });
+  });
+
+  it("parses a logical expression with multiple parentheses", () => {
+    expect(LogicalExpressionAST.from("(1 AND 2) OR (2 AND 3)")).toEqual({
+      type: "OR",
+      left: {
         type: "AND",
         left: {
           type: "literal",
@@ -20,101 +75,44 @@ describe("LogicalExpressionAST", () => {
           type: "literal",
           value: 2,
         },
-      });
-    });
-
-    it("parses a simple OR logic string", () => {
-      expect(LogicalExpressionAST.from("1 OR 2")).toEqual({
-        type: "OR",
+      },
+      right: {
+        type: "AND",
         left: {
-          type: "literal",
-          value: 1,
-        },
-        right: {
           type: "literal",
           value: 2,
         },
-      });
-    });
-
-    it("parses multiple OR", () => {
-      expect(LogicalExpressionAST.from("1 OR 2 OR 3")).toEqual({
-        type: "OR",
-        left: { type: "literal", value: 1 },
         right: {
-          left: { type: "literal", value: 2 },
-          right: { type: "literal", value: 3 },
+          type: "literal",
+          value: 3,
+        },
+      },
+    });
+  });
+
+  it("parses a logical expression with nested parentheses", () => {
+    expect(
+      LogicalExpressionAST.from("((1 OR 3) AND 2) OR ((1 OR 3) AND 4)")
+    ).toEqual({
+      type: "OR",
+      left: {
+        type: "AND",
+        left: {
           type: "OR",
+          left: { type: "literal", value: 1 },
+          right: { type: "literal", value: 3 },
         },
-      });
-    });
-
-    it("parses a simple logic string with parentheses", () => {
-      expect(LogicalExpressionAST.from("(1 OR 2)")).toEqual({
-        type: "OR",
+        right: { type: "literal", value: 2 },
+      },
+      right: {
+        type: "AND",
         left: {
-          type: "literal",
-          value: 1,
+          type: "OR",
+          left: { type: "literal", value: 1 },
+          right: { type: "literal", value: 3 },
         },
-        right: {
-          type: "literal",
-          value: 2,
-        },
-      });
-    });
-
-    it("parses a logic string with multiple parentheses", () => {
-      expect(LogicalExpressionAST.from("(1 AND 2) OR (2 AND 3)")).toEqual({
-        type: "OR",
-        left: {
-          type: "AND",
-          left: {
-            type: "literal",
-            value: 1,
-          },
-          right: {
-            type: "literal",
-            value: 2,
-          },
-        },
-        right: {
-          type: "AND",
-          left: {
-            type: "literal",
-            value: 2,
-          },
-          right: {
-            type: "literal",
-            value: 3,
-          },
-        },
-      });
-    });
-
-    it("parses a logic string with nested parentheses", () => {
-      expect(
-        LogicalExpressionAST.from("((1 OR 3) AND 2) OR ((1 OR 3) AND 4)")
-      ).toEqual({
-        type: "OR",
-        left: {
-          type: "AND",
-          left: {
-            type: "OR",
-            left: { type: "literal", value: 1 },
-            right: { type: "literal", value: 3 },
-          },
-          right: { type: "literal", value: 2 },
-        },
-        right: {
-          type: "AND",
-          left: {
-            type: "OR",
-            left: { type: "literal", value: 1 },
-            right: { type: "literal", value: 3 },
-          },
-          right: { type: "literal", value: 4 },
-        },
-      });
+        right: { type: "literal", value: 4 },
+      },
     });
   });
 
